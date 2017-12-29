8ac6a2e9a538037b511b3ffc1b21c4f20695470b
muratzorer <muratzorer35@gmail.com>
2017-12-29 15:32:41 +0300

    now ignoring ssl trust

--- scripts/vagrant-ssh.bat
+++ scripts/vagrant-ssh.bat
@@ -2,5 +2,7 @@
 if exist a:\vagrant.pub (
   copy a:\vagrant.pub C:\Users\vagrant\.ssh\authorized_keys
 ) else (
-  powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub', 'C:\Users\vagrant\.ssh\authorized_keys')" <NUL
+  mkdir "C:\Users\vagrant\.ssh"
+  [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+  powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub', 'C:\Users\vagrant\.ssh\authorized_keys')" <NUL
 )

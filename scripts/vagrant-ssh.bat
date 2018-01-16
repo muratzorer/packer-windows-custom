05abd3741290ff877a2f7bfb0ab00efe7143727b
muratzorer <muratzorer35@gmail.com>
2018-01-16 12:18:05 +0300

    certificate validation turned off before download

--- scripts/vagrant-ssh.bat
+++ scripts/vagrant-ssh.bat
@@ -3,6 +3,6 @@ if exist a:\vagrant.pub (
   copy a:\vagrant.pub C:\Users\vagrant\.ssh\authorized_keys
 ) else (
   mkdir "C:\Users\vagrant\.ssh"
-  [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+  powershell [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true} <NUL
   powershell -Command "(New-Object System.Net.WebClient).DownloadFile('http://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub', 'C:\Users\vagrant\.ssh\authorized_keys')" <NUL
 )
